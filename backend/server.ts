cf1784092adc785809daeb582b36651e93cfbefd
unknown <mathis.jankovic@gmail.com>
2024-03-12 01:33:21 +0100

    Add basic logs for db connection & auto reconnection after delay

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,6 @@
-import express from 'express'
 import mongoose from 'mongoose'
+import express from 'express'
+import fs from 'fs'
 
 import login from './routes/auth/login'
 import register from './routes/auth/register'
@@ -13,6 +14,8 @@ const dbPassword: string = process.env.DB_PASSWORD || ''
 const dbUser: string  = process.env.DB_USER || 'root'
 const dbName: string = process.env.DB_NAME || 'shopingreen'
 
+const settings = JSON.parse(fs.readFileSync('./settings.json').toString())
+
 const app = express()
 
 // Middlewares
@@ -25,8 +28,42 @@ app.use('/register', register)
 app.use('/user', getUserDetails)
 app.use('/user/update', updateUser)
 
-mongoose.connect(`mongodb://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}?authSource=admin`)
+let hasConnectedToDb = false
+mongoose.connection.on('connected', () => {
+    console.log('successfully connected to MongoDB')
+    hasConnectedToDb = true
+})
+
+let isConnecting = false
+
+async function connectDbWithRetry() {
+    if (isConnecting) return
+    isConnecting = true
+
+    while(true) {
+        try {
+            await mongoose.connect(`mongodb://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}?authSource=admin`)
+            break
+        } catch (error) {
+            console.error(`failed to connect to MongoDB, retrying in ${settings.dbReconnectDelaySec} sec`)
+            await new Promise(resolve => setTimeout(resolve, settings.dbReconnectDelaySec * 1000))
+        }
+    }
+
+    isConnecting = false
+}
+(async () => {
+    await connectDbWithRetry()
+})()
+
+mongoose.connection.on('disconnected', () => {
+    if(hasConnectedToDb) {
+        console.log('disconnected from MongoDB, reconnecting...')
+        hasConnectedToDb = false
+    }
+    connectDbWithRetry()
+})
 
 app.listen(serverPort, () => {
-    console.log('Server is ready')
+    console.log('express server is up and running')
 })
