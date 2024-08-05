6011e809b62fb64c8e9f9d407176afe600aa8ef1
linjacques <linjacques463@gmail.com>
2024-08-05 13:37:48 +0200

    add components Product

--- app/frontend/src/components/Product.tsx
+++ app/frontend/src/components/Product.tsx
@@ -9,19 +9,18 @@ interface ProductProps {
 
 const Product: React.FC<ProductProps> = ({ image, name, price, rating }) => {
   return (
-    <div className="bg-white rounded-lg shadow-md overflow-hidden w-full sm:w-60 m-4">
-    <img src={image} alt={name} className="w-full h-32 object-cover " />
-    <div className="p-4">
-      <h3 className="text-lg font-bold mb-2 text-[#A07E53]">{name}</h3>
-      <p className="text-gray-600">{price}</p>
-      {rating && (
-        <div className="mt-2 flex items-center">
-          <span>{rating}</span> <span className="text-yellow-500 ml-1">⭐</span>
-        </div>
-      )}
+    <div className="bg-white rounded-lg shadow-md overflow-hidden w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 m-4">
+      <img src={image} alt={name} className="w-full h-32 object-cover" />
+      <div className="p-4">
+        <h3 className="text-lg font-bold mb-2 text-[#A07E53]">{name}</h3>
+        <p className="text-gray-600">{price}</p>
+        {rating && (
+          <div className="mt-2 flex items-center">
+            <span>{rating}</span> <span className="text-yellow-500 ml-1">⭐</span>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-  
   );
 };
 
