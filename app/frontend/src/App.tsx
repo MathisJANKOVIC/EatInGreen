20336747f256786699709493633f17624c4e08e6
Guillaume1j <Guillaume.juy02@gmail.com>
2024-08-06 13:30:27 +0200

    Fix Routing Frontend

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,15 +1,21 @@
-import { Outlet } from "react-router-dom"
-import Header from "./components/header/header";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Main from './Main.tsx'
+import './tailwind.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './pages/auth/Login.tsx'
+import Register from './pages/auth/Register.tsx'
+import Home from './pages/home/Home.tsx'
 
-function App() {
-  return (
-    <div className="w-screen h-screen bg-customWhite flex justify-center items-center">
-      <Header/>
-      <Outlet/>
+const router = createBrowserRouter([
+  {path: "/login",element: <Login/>},
+  {path: "/Register",element: <Register/>},
+  {path: "/",element: <Main/>,children: [{path: "/",element: <Home/>}]}
+])
 
-    </div>
-  ); 
-}
-
-export default App
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
