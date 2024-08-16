e5f49709100ce3304888e5291c28d5478f371de7
unknown <mathis.jankovic@gmail.com>
2024-08-16 13:06:11 +0200

    Change indentation to 2 for config backend files

--- app/backend/jest.config.mjs
+++ app/backend/jest.config.mjs
@@ -1,9 +1,11 @@
 export default {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/tests/**/*.test.ts'],
-    setupFilesAfterEnv: ['./tests/setup.ts'],
-    forceExit: true,
-    detectOpenHandles: true,
-    transform: { '^.+\\.ts$': ['ts-jest', { tsconfig: './tsconfig.json' }] },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  setupFilesAfterEnv: ['./tests/setup.ts'],
+  forceExit: true,
+  detectOpenHandles: true,
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { tsconfig: './tsconfig.json' }]
+  }
 }
