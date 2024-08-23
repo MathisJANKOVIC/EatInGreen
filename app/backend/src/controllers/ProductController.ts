97df467ee45206da38ce0d7a2ca7a9fc7db35fe6
Rasting <mathis.jankovic@gmail.com>
2024-08-23 15:40:03 +0200

    Remove useless try catch in ProductConstroller

--- app/backend/src/controllers/ProductController.ts
+++ app/backend/src/controllers/ProductController.ts
@@ -1,5 +1,5 @@
-import ProductService from '@services/ProductService'
 import ProductFactory from '@factories/ProductFactory'
+import ProductService from '@services/ProductService'
 import HTTPError from '@utils/HTTPError'
 
 import { Request, Response } from 'express'
@@ -19,98 +19,71 @@ class ProductController {
     }
 
     public async getProductById(req: Request, res: Response): Promise<void> {
-        try {
-            const { id } = req.params
+        const { id } = req.params
 
-            if (!id) {
-                throw new HTTPError(400, 'Product ID is required')
-            }
-
-            const product = await this.productService.getProductById(id)
-            if (!product) {
-                throw new HTTPError(404, 'Product not found')
-            }
-            res.status(200).json(product)
-        } catch (error) {
-            console.error('Error retrieving product by ID:', error)
-            throw new HTTPError(500, 'Failed to retrieve product by ID')
+        if (!id) {
+            throw new HTTPError(400, 'Product ID is required')
         }
+
+        const product = await this.productService.getProductById(id)
+        if (!product) {
+            throw new HTTPError(404, 'Product not found')
+        }
+        res.status(200).json(product)
     }
 
     public async getProductByName(req: Request, res: Response): Promise<void> {
-        try {
-            const name: string | undefined = req.params.name
+        const name: string | undefined = req.params.name
 
-            if (!name) {
-                throw new HTTPError(400, 'Product name is required')
-            }
-
-            const product = await this.productService.getProductByName(name)
-            if (!product) {
-                throw new HTTPError(404, 'Product not found')
-            }
-            res.status(200).json(product)
-        } catch (error) {
-            console.error('Error retrieving product by name:', error)
-            throw new HTTPError(500, 'Failed to retrieve product')
+        if (!name) {
+            throw new HTTPError(400, 'Product name is required')
         }
+
+        const product = await this.productService.getProductByName(name)
+        if (!product) {
+            throw new HTTPError(404, 'Product not found')
+        }
+        res.status(200).json(product)
     }
 
     public async getAllProducts(req: Request, res: Response): Promise<void> {
-        try {
-            const limit = parseInt(req.query.limit as string) || 30
-            const products = await this.productService.getAllProduct(limit)
-            if (!products || products.length === 0) {
-                throw new HTTPError(404, 'No products found')
-            }
-            res.status(200).json(products)
-        } catch (error) {
-            console.error('Error retrieving all products:', error)
-            throw new HTTPError(500, 'Failed to retrieve products')
+        const limit = parseInt(req.query.limit as string) || 30
+        const products = await this.productService.getAllProduct(limit)
+        if (!products || products.length === 0) {
+            throw new HTTPError(404, 'No products found')
         }
+        res.status(200).json(products)
     }
 
     public async updateProduct(req: Request, res: Response): Promise<void> {
-        try {
-            const { id } = req.params
-            const updatedData = req.body
+        const { id } = req.params
+        const updatedData = req.body
 
-            if (!id) {
-                throw new HTTPError(400, 'Product ID is required')
-            }
-
-            const updatedProduct = await this.productService.updateProduct(id, updatedData)
-            if (!updatedProduct) {
-                throw new HTTPError(404, 'Product not found')
-            }
-
-            res.status(200).json(updatedProduct)
-        } catch (error) {
-            console.error('Error updating product:', error)
-            throw new HTTPError(500, 'Failed to update product')
+        if (!id) {
+            throw new HTTPError(400, 'Product ID is required')
         }
+
+        const updatedProduct = await this.productService.updateProduct(id, updatedData)
+        if (!updatedProduct) {
+            throw new HTTPError(404, 'Product not found')
+        }
+        res.status(200).json(updatedProduct)
     }
 
     public async deleteProduct(req: Request, res: Response): Promise<void> {
-        try {
-            const { id } = req.params
+        const { id } = req.params
 
-            if (!id) {
-                throw new HTTPError(400, 'Product ID is required')
-            }
-
-            const deleted = await this.productService.deleteProduct(id)
-            if (!deleted) {
-                throw new HTTPError(404, 'Product not found')
-            }
-
-            res.status(200).json({ message: 'Product successfully deleted' })
-        } catch (error) {
-            console.error('Error deleting product:', error)
-            throw new HTTPError(500, 'Failed to delete product')
+        if (!id) {
+            throw new HTTPError(400, 'Product ID is required')
         }
-    }
 
+        const deleted = await this.productService.deleteProduct(id)
+        if (!deleted) {
+            throw new HTTPError(404, 'Product not found')
+        }
+
+        res.status(200).json({ message: 'Product successfully deleted' })
+    }
 }
 
 export default ProductController
