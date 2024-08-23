bb02407c3f54426e747b3bbf77731ceaa97b0ef8
Rasting <mathis.jankovic@gmail.com>
2024-08-23 16:04:24 +0200

    Add test for update profile route

---
+++ app/backend/tests/routes/user/updateProfile.test.ts
@@ -0,0 +1,29 @@
+import { client } from '../../setup'
+
+describe('User routes', () => {
+    test('update a user profile', async () => {
+        const userData = {
+            firstName: 'John',
+            lastName: 'Doe',
+            phoneNumber: '+1 123 456 7890',
+            email: 'john@gmail.com',
+            password: 'john123'
+        }
+        await client.post('/auth/register', userData)
+
+        const newUserData = {
+            firstName: 'Jane',
+            lastName: 'Smith',
+            phoneNumber: '+2 123 456 7890'
+        }
+        const response = await client.patch('/user/profile', newUserData)
+        expect(response.status).toBe(200)
+
+        expect(response.body).toHaveProperty('user')
+        const responseUser = response.body.user
+        expect(responseUser.firstName).toBe(newUserData.firstName)
+        expect(responseUser.lastName).toBe(newUserData.lastName)
+        expect(responseUser.email).toBe(userData.email)
+        expect(responseUser.phoneNumber).toBe(newUserData.phoneNumber)
+    })
+})
