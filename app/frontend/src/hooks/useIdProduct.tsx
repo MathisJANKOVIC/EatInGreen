71c9ac06b76132ed8b8f4573366b3c63c9bb5b7e
linjacques <linjacques463@gmail.com>
2024-08-24 20:17:43 +0200

    fix useIdProduct

--- app/frontend/src/hooks/useIdProduct.tsx
+++ app/frontend/src/hooks/useIdProduct.tsx
@@ -23,7 +23,7 @@ const useIdProduct = (_id: string | undefined) => {
     const fetchProduct = async () => {
       try {
         setLoading(true)
-        const res = await fetch(`/api/product/products/id/${_id}`, {
+        const res = await fetch(`/api/product/products/${_id}`, {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
