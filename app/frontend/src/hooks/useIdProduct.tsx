8de99b48d52fea6734cfbcdf8e9306e50723387f
linjacques <linjacques463@gmail.com>
2024-08-24 20:15:27 +0200

    Fix issue due to API route being changed

--- app/frontend/src/hooks/useIdProduct.tsx
+++ app/frontend/src/hooks/useIdProduct.tsx
@@ -23,7 +23,7 @@ const useIdProduct = (_id: string | undefined) => {
     const fetchProduct = async () => {
       try {
         setLoading(true)
-        const res = await fetch(`/api/product/product/id/${_id}`, {
+        const res = await fetch(`/api/product/products/id/${_id}`, {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
