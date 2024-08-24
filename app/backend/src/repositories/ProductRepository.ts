6c32572e249486c440b3591514898f7833b90eed
unknown <mathis.jankovic@gmail.com>
2024-08-24 18:53:28 +0200

    Remove useless product route & rename endpoints

--- app/backend/src/repositories/ProductRepository.ts
+++ app/backend/src/repositories/ProductRepository.ts
@@ -4,7 +4,6 @@ import { ProductDTO } from '@types-dto/productDTO'
 interface ProductRepository extends EntityRepository<ProductDTO> {
     findByName(name: string): Promise<ProductDTO | null>
     findAll(limit: number): Promise<ProductDTO[]>
-    delete(id: string): Promise<ProductDTO | null>
 }
 
 export default ProductRepository
--- app/backend/src/routers/productRouter.ts
+++ app/backend/src/routers/productRouter.ts
@@ -6,13 +6,11 @@ import { Router } from 'express'
 
 const productRouter = Router()
 
-const prodController = new ProductController(productService)
-productRouter.post('/product', asyncHandler(prodController.createProduct.bind(prodController)))
-productRouter.get('/products', asyncHandler(prodController.getAllProducts.bind(prodController)))
-productRouter.get('/product/name/:name', asyncHandler(prodController.getProductByName.bind(prodController)))
-productRouter.get('/product/id/:id', asyncHandler(prodController.getProductById.bind(prodController)))
-productRouter.put('/product/:id', asyncHandler(prodController.updateProduct.bind(prodController)))
-productRouter.delete('/product/:id', asyncHandler(prodController.deleteProduct.bind(prodController)))
+const productController = new ProductController(productService)
 
+productRouter.get('/products', asyncHandler(productController.getAllProducts.bind(productController)))
+productRouter.get('/products/:id', asyncHandler(productController.getProductById.bind(productController)))
+productRouter.get('/products/:name', asyncHandler(productController.getProductByName.bind(productController)))
+productRouter.post('/products', asyncHandler(productController.createProduct.bind(productController)))
 
 export default productRouter
